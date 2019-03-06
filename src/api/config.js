5df32ef5e5074c8bf877fb4b6d0e5938d497721b
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2019-03-06 11:13:20 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appname: 'Platzi Music',
+    apiKey: '32366cc14727d210369432c533f37528',
+    secret: '3ea95f313fccdd7e2391c912ab9863ec',
+    registerTo: 'NAIJULU'
+}
+
+// Application name	Platzi Music
+// API key	32366cc14727d210369432c533f37528
+// Shared secret	3ea95f313fccdd7e2391c912ab9863ec
+// Registered to	NAIJULU
